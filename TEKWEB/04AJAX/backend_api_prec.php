bdb1d6a306de5d7d1ffafb3cced3e17b6ea6a99c
paolo <paolo@foo.com>
2026-03-08 22:08:22 +0100

    aggiunge esempio con Ajax

---
+++ TEKWEB/04AJAX/backend_api_prec.php
@@ -0,0 +1,22 @@
+<?php
+    //legge la directory galleria e salva i nomi dei file in un array
+    $files = scandir("../galleria");
+    $files = array_diff($files, array('.', '..')); //rimuove i file . e .. dalla lista
+    $files = array_values($files); //reindicizza l'array dopo aver rimosso i file . e .. 
+    $n=count($files);
+    
+    $indice = (int)$_GET["indice"];  //TODO: inserire controllo su parametro
+    $indice = indice(-1);
+
+    header("content-type: text/text");
+    
+    echo $indice . " " . $files[$indice];
+
+
+
+    function indice( $direzione ){  
+        global $indice,$n;
+        return ($indice + $direzione + $n ) % $n;
+
+    }
+?>
